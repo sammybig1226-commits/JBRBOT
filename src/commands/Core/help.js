981173ceb79426ccc7ca13706aee2381a9a710c0
sammybig1226-commits <sammybig1226@gmail.com>
2026-09-21 11:21:47 +0300

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -119,7 +119,7 @@ export async function createInitialHelpMenu(client) {
 
     const supportButton = new ButtonBuilder()
         .setLabel("Support Server")
-        .setURL("https://discord.gg/QnWNz2dKCE")
+        .setURL("https://discord.gg/mFPBDN3hJS")
         .setStyle(ButtonStyle.Link);
 
     const selectRow = createSelectMenu(
